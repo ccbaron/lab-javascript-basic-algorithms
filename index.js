092e9fcb9870e0ebdab8c4917cd4820d5352b157
ccbaron <christian7baron@gmail.com>
2025-03-12 19:41:29 +0100

    Iteración 1

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // Iteration 1: Names and Input
 
+let driver = "Christian";
+let navigator = "Javier";
+
+console.log(`[iteration 1]: The driver's name is ${driver}`);
+console.log(`[iteration 1]: The navigator's name is ${navigator}`);
 
 // Iteration 2: Conditionals
+
+
+
+
 
 
 // Iteration 3: Loops
