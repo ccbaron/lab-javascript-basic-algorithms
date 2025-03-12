623723e47e570be620bf36bd7b463d1673dbd837
ccbaron <christian7baron@gmail.com>
2025-03-12 19:48:41 +0100

    iteración 2

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ console.log(`[iteration 1]: The navigator's name is ${navigator}`);
 
 // Iteration 2: Conditionals
 
-
+if (driver.length > navigator.length) {
+  console.log(
+    `[iteration 2]: The driver has the longest name, it has ${driver.length} characters.`
+  );
+}
+else if (driver.length < navigator.length) {
+  console.log(
+    `[iteration 2]: It seems that the navigator has the longest name, it has ${navigator.length} characters.`
+  );
+}
 
 
 
